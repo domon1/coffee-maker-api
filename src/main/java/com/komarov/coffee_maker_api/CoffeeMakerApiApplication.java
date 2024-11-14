efbb2dba135aa323ddd6516336ffe26c4170ce62
domon1 <dimoniy.dota2@mail.ru>
2024-11-14 22:58:29 +0300

    Initial commit

---
+++ src/main/java/com/komarov/coffee_maker_api/CoffeeMakerApiApplication.java
@@ -0,0 +1,13 @@
+package com.komarov.coffee_maker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeMakerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeMakerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/komarov/coffee_maker_api/CoffeeMakerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komarov.coffee_maker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeMakerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
